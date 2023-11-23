fa9a77283c89992a75be7acb138e2c94500888a8
CodeSandbox <support@codesandbox.io>
2023-11-23 05:28:41 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello CodeSandbox!")
+from discord.ext import commands
+import discord
+import os
+# python3 - m pip install - U discord.py
+
+
+intents = discord.Intents.default()
+discord.Intents.all()
+client = discord.Client(intents=intents)
+
+my_secret = os.environ['TOKEN']
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('rodou! {0.user}'.format(client))
+
+
+@bot.command(name='win')
+async def win(ctx):
+    print("Chamou")
+    await ctx.send('ok!')
+
+client.run(my_secret)
