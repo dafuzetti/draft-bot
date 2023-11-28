cd82c8111b3d0334b63cb680fdab78c4e4882804
CodeSandbox <support@codesandbox.io>
2023-11-28 15:28:45 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
+import discord #import all the necessary modules
+from discord import Game
 from discord.ext import commands
-import discord
 import os
-# python3 - m pip install - U discord.py
-
-
-intents = discord.Intents.default()
-discord.Intents.all()
-client = discord.Client(intents=intents)
 
 my_secret = os.environ['TOKEN']
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!') #define command decorator
 
+@bot.command(pass_context=True) #define the first command and set prefix to '!'
+async def testt(ctx):
+    await ctx.send('Hello!!')
 
-@client.event
+@bot.event #print that the bot is ready to make sure that it actually logged on
 async def on_ready():
-    print('rodou! {0.user}'.format(client))
+    print('Logged in as:')
+    print(bot.user.name)
+    await bot.change_presence(game=Game(name="in rain ¬ !jhelp"))
 
-
-@bot.command(name='win')
-async def win(ctx):
-    print("Chamou")
-    await ctx.send('ok!')
-
-client.run(my_secret)
+bot.run(TOKEN) #run the client using using my bot's token
