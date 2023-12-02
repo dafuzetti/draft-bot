029946581390492dc11d79b9b666c562e107cc1b
CodeSandbox <support@codesandbox.io>
2023-12-02 22:30:40 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import discord
+from discord import app_commands
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext, cog_ext
 from discord_slash.model import SlashCommandOptionType
+import discord
 import itertools
 import pandas
 import numpy
